4243d360c647ec1f6146bf04843cc2e214bf3298
supergold <ittechnology1029@hotmail.com>
2022-11-16 20:47:42 +0100

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+export const applyCustomCode = (externalCodeSetup: any) => {
+	externalCodeSetup.cssApi.addGlobalStyle("vc-reshare-border", {
+		borderWidth: "2",
+		borderColor: "#23ff56",
+		shadowColor: "#23ff56",
+		shadowOpacity: 0.8,
+		shadowRadius: 2,
+		shadowOffset: {
+		height: 1,
+		width: 1
+		}
+	});
+};
