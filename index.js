1cf9d2ce54578592e382da8ed896ac98479bd616
supergold <ittechnology1029@hotmail.com>
2022-11-21 01:26:32 +0100

    avatar - test2

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 export const applyCustomCode = (externalCodeSetup: any) => {
 	externalCodeSetup.cssApi.addGlobalStyle("avatar", {
-		borderWidth: "2",
-		borderColor: "#23ff56",
-		shadowColor: "#23ff56",
-		shadowOpacity: 0.8,
-		shadowRadius: 2,
-		shadowOffset: {
-		height: 1,
-		width: 1
-		}
-	});
+		
 };
