1ec1c8ce297dc0303f9eb80add47c1c4d8b5ff94
supergold <ittechnology1029@hotmail.com>
2022-11-21 08:11:48 +0100

    test2- itemavatar-style

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-import ActivityHeader from "./ActivityHeader";
-export const applyCustomCode = externalCodeSetup => {
-  externalCodeSetup.activitiesScreenApi.setActivityHeaderComponent(props => <ActivityHeader {...props} />)
-}
+export const applyCustomCode = (externalCodeSetup: any) => {
+	externalCodeSetup.cssApi.addGlobalStyle("itemAvatar" ,{
+		marginRight: 10,
+		width: 32,
+		height: 32,
+		borderRadius: 15,
+		backgroundColor: "transparent",
+		borderWidth: "2",
+		borderColor: "#23ff56",
+		shadowColor: "#23ff56",
+		shadowOpacity: 0.8,
+		shadowRadius: 2,
+		shadowOffset: {
+		height: 1,
+		width: 1
+		}
+	});
+};
